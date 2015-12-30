5e72dbafc21240b80af8298c8c7a04db34b7fccf
Illia Rahavoi <irahavoi@gmail.com>
2015-12-29 22:18:52 -0600

    Fixed Images displaying issues

--- XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
@@ -3,6 +3,8 @@ package com.example.xyzreader.remote;
 import android.util.Log;
 
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,15 +47,17 @@ public class RemoteEndpointUtil {
     }
 
     static String fetchPlainText(URL url) throws IOException {
-        return new String(fetch(url), "UTF-8" );
-    }
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder() .url(url) .build();
+        Response response = client.newCall(request).execute(); return response.body().string(); }
 
     static byte[] fetch(URL url) throws IOException {
         InputStream in = null;
 
         try {
             OkHttpClient client = new OkHttpClient();
-            HttpURLConnection conn = client.open(url);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             in = conn.getInputStream();
             byte[] buffer = new byte[1024];
